49e74051cc6ed1c0249cd873d77fc253a06ac0db
Adeesha Sandun <adeeshasandun.as@gmail.com>
2025-10-08 14:54:41 +0530

    Labs data load issue fix FMSP & PMSP

--- presentation/pmsp/renewal/masterData/instituteFacility/instituteFacility-db-set.php
+++ presentation/pmsp/renewal/masterData/instituteFacility/instituteFacility-db-set.php
@@ -31,6 +31,7 @@ $cboEmgKit = isset($_REQUEST['cboEmgKit'])?trim($_REQUEST['cboEmgKit']):null;
 $requestType 	= $_REQUEST['requestType'];
 $anStatus       = $_REQUEST['anStatus'];
 $id             = $_REQUEST['cboSearch'];
+$instituteId             = $_REQUEST['cboSearch'];
 
 $facilityDetail = json_decode($_REQUEST['facilityDetail'], true);
 
@@ -103,7 +104,7 @@ if($requestType=='edit'){
     if($detailResult){                    
         $response['type'] 	= 'pass';
         $response['msg'] 	= 'Facilities saved successfully! Proceed to Documents...';
-        $response['id'] 	= $entryId;
+        $response['id'] 	= $instituteId;
         $db->commit();
         // commit auto number
 //        $clsAutoNo->setAutoNoCommit($autoNoType, $autoNo);
